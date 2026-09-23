7f4a7bd20650979d874680bac53e137bdb25b411
Andrei Iatsuk <5247416+iatsuk@users.noreply.github.com>
2026-09-23 11:25:39 +0200

    Add forecabin comfort and boom furling

--- i18n.js
+++ i18n.js
@@ -3,8 +3,8 @@
   const storageKey = 'aurora-language';
   const languageNames = { en: 'English', de: 'Deutsch', ru: 'Русский' };
   const translationFiles = {
-    de: ['i18n-de.js?v=20260923-1'],
-    ru: ['i18n-ru.js?v=20260923-1']
+    de: ['i18n-de.js?v=20260923-2'],
+    ru: ['i18n-ru.js?v=20260923-2']
   };
 
   const normalizeLanguage = (value) => {
